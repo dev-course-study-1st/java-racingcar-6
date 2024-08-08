54fad0fea18a9b015ad9ffd2269aba330c1619ef
jmd5314 <jmd5314@gmail.com>
2024-08-09 03:57:50 +0900

    feat: 컨트롤러 구현

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,4 +1,29 @@
 package racingcar.controller;
 
+import racingcar.model.Cars;
+import racingcar.model.RoundStatus;
+import racingcar.service.RaceService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class RaceController {
+    private final RaceService raceService;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RaceController(RaceService raceService, InputView inputView, OutputView outputView) {
+        this.raceService = raceService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start(){
+        Cars cars = inputView.inputCarNames();
+        int attemptCount = inputView.getAttemptCount();
+        for (int i = 0; i < attemptCount; i++) {
+            RoundStatus roundStatus = raceService.race(cars);
+            outputView.printRaceStatus(roundStatus);
+        }
+        outputView.printWinners(raceService.getWinners(cars));
+    }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,24 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Cars;
+import racingcar.utils.converter.CarListConverter;
 import racingcar.utils.enums.GameMessage;
 import racingcar.utils.validator.Validator;
 
 public class InputView {
     private final Validator<String[]> nameValidator;
     private final Validator<Integer> numberValidator;
+    private final CarListConverter carListConverter;
 
-    public InputView(Validator<String[]> nameValidator, Validator<Integer> numberValidator) {
+    public InputView(Validator<String[]> nameValidator, Validator<Integer> numberValidator, CarListConverter carListConverter) {
         this.nameValidator = nameValidator;
         this.numberValidator = numberValidator;
+        this.carListConverter = carListConverter;
+    }
+    public Cars inputCarNames(){
+        String[] carNames = getCarNames();
+        return carListConverter.convert(carNames);
     }
 
     public String[] getCarNames() {
