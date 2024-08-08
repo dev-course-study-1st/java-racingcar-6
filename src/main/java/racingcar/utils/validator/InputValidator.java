2226182d6efa1f91c94940a846c5136bf82344b8
NaMinhyeok <nmh9097@gmail.com>
2024-08-08 18:20:38 +0900

    feat : 자동차 이름 5글자 이하 검증 구현

--- src/main/java/racingcar/utils/validator/InputValidator.java
+++ src/main/java/racingcar/utils/validator/InputValidator.java
@@ -1,17 +1,25 @@
 package racingcar.utils.validator;
 
-public class InputValidator {
-    private final String IS_INTEGER_PATTERN = "^[0-9]*$";
+import racingcar.utils.constant.GameNumber;
 
-    public void isInteger(String inputString) {
+public class InputValidator {
+    private final static String IS_INTEGER_PATTERN = "^[0-9]*$";
+
+    public static void isInteger(String inputString) {
         if (!inputString.matches(IS_INTEGER_PATTERN)) {
             throw new IllegalArgumentException("정수가 아닙니다.");
         }
     }
 
-    public void isCarNameInput(String inputString) {
+    public static void isCarNameInput(String inputString) {
         if (!inputString.matches("^[a-zA-Z,]*$")) {
             throw new IllegalArgumentException("영문자와 쉼표만 입력해주세요.");
+        }
+    }
+
+    public static void isCarNameLength(String inputString) {
+        if (inputString.length() > GameNumber.MAX_NAME_LENGTH.getNumber()) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
     }
 }
