11b7652ab0eeae95d9961a2e0beaeb7c5020daf0
NaMinhyeok <nmh9097@gmail.com>
2024-08-08 16:28:17 +0900

    feat : Model 객체 자동차, 자동차 목록 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,22 @@
+package racingcar.model;
+
+import racingcar.utils.constant.GameNumber;
+
+public class Car {
+    private final String name;
+    private Integer position;
+
+    public Car(String carName) {
+        this.name = carName;
+        this.position = GameNumber.DEFAULT_FORWARD_COUNT.getNumber();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+}
+
