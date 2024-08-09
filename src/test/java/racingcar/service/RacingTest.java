ff49fc98ac464f569ca072c2bcf411129b35ed88
junho <>
2024-08-09 16:42:52 +0900

    test: 우승자가 2명 이상일 경우에 대한 테스트 코드 추가 - 성공

---
+++ src/test/java/racingcar/service/RacingTest.java
@@ -0,0 +1,32 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+import racingcar.util.constant.NumberEnum;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingTest extends NsTest {
+
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
+    @Test
+    @DisplayName("우승자가 2명 이상일 때")
+    void winnerIsOverThanTwo() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,yoo", "2");
+                    assertThat(output()).contains("pobi : --", "woni : --", "yoo : ", "최종 우승자 : pobi,woni");
+                }, NumberEnum.FOUR.getValue(), NumberEnum.FOUR.getValue(), NumberEnum.THREE.getValue()
+                , NumberEnum.FOUR.getValue(), NumberEnum.FOUR.getValue(), NumberEnum.THREE.getValue()
+        );
+    }
+
+}
