dc562d8a6e89d9d9b65cb03a465b72aec707ee05
jmd5314 <jmd5314@gmail.com>
2024-08-09 20:47:37 +0900

    test: 우승자 계산 테스트 작성

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -3,16 +3,14 @@ package racingcar.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import racingcar.utils.enums.GameNumber;
 import racingcar.utils.generator.FixedNumberGenerator;
-import racingcar.utils.generator.NumberGenerator;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarsTest {
     private Car car1;
@@ -22,9 +20,9 @@ class CarsTest {
 
     @BeforeEach
     void setUp() {
-        car1 = new Car(new Name("조믿음"), new Position(GameNumber.START_POSITION.getNumber()));
-        car2 = new Car(new Name("이철수"), new Position(GameNumber.START_POSITION.getNumber()));
-        car3 = new Car(new Name("김영희"), new Position(GameNumber.START_POSITION.getNumber()));
+        car1 = new Car(new Name("Car1"), new Position(GameNumber.START_POSITION.getNumber()));
+        car2 = new Car(new Name("Car2"), new Position(GameNumber.START_POSITION.getNumber()));
+        car3 = new Car(new Name("Car3"), new Position(GameNumber.START_POSITION.getNumber()));
 
         List<Car> carList = Arrays.asList(car1, car2, car3);
         cars = new Cars(carList);
@@ -41,7 +39,7 @@ class CarsTest {
     @Test
     public void checkNameList(){
         List<String> nameList = cars.carToString();
-        assertEquals(List.of("조믿음", "이철수", "김영희"), nameList);
+        assertEquals(List.of("Car1", "Car2", "Car3"), nameList);
     }
 
     @DisplayName("차 위치 맵 반환 테스트")
@@ -49,9 +47,9 @@ class CarsTest {
     public void checkMap(){
         Map<String, Integer> result = cars.carToMap();
         assertEquals(3, result.size());
-        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("조믿음"));
-        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("이철수"));
-        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("김영희"));
+        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("Car1"));
+        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("Car2"));
+        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("Car3"));
     }
 
     @DisplayName("4미만 정지 후 맵 반환 테스트")
@@ -59,9 +57,9 @@ class CarsTest {
     public void checkStop(){
         cars.moveCars(new FixedNumberGenerator(3));
         Map<String, Integer> result = cars.carToMap();
-        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("조믿음"));
-        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("이철수"));
-        assertEquals(GameNumber.START_POSITION.getNumber(), result.get("김영희"));
+        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("Car1"));
+        assertEquals(GameNumber.START_POSITION.getNumber(),result.get("Car2"));
+        assertEquals(GameNumber.START_POSITION.getNumber(), result.get("Car3"));
     }
 
     @DisplayName("4이상 이동 후 맵 반환 테스트")
@@ -69,9 +67,10 @@ class CarsTest {
     public void checkMove(){
         cars.moveCars(new FixedNumberGenerator(5));
         Map<String, Integer> result = cars.carToMap();
-        assertEquals(GameNumber.START_POSITION.getNumber()+1,result.get("조믿음"));
-        assertEquals(GameNumber.START_POSITION.getNumber()+1,result.get("이철수"));
-        assertEquals(GameNumber.START_POSITION.getNumber()+1, result.get("김영희"));
+        assertEquals(GameNumber.START_POSITION.getNumber()+1,result.get("Car1"));
+        assertEquals(GameNumber.START_POSITION.getNumber()+1,result.get("Car2"));
+        assertEquals(GameNumber.START_POSITION.getNumber()+1, result.get("Car3"));
 
     }
+
 }
